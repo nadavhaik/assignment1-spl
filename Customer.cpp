852b3ddde4a3f58e7ac39fd4d8aaee0f6c1fab67
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2021-11-09 18:47:38 +0200

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -16,10 +16,11 @@ SweatyCustomer::SweatyCustomer(std::string name, int id) : Customer(name, id) {
 
 }
 
-std::vector<int> SweatyCustomer::order(const std::vector<Workout> &workout_options) {
+std::vector<int> SweatyCustomer::order(const std::vector<Workout> &workout_options){
     std::vector<int> options;
-    for(Workout w : workout_options) {
-
+    for(const Workout& w : workout_options) {
+        if(w.getType() == WorkoutType::CARDIO)
+            options.push_back(w.getId());
     }
     return options;
 }
@@ -28,12 +29,30 @@ CheapCustomer::CheapCustomer(std::string name, int id) : Customer(name, id) {
 
 }
 
+std::vector<int> CheapCustomer::order(const std::vector<Workout> &workout_options) {
+    size_t cheapest_index = 0;
+    for(size_t i = 1; i<workout_options.size(); i++) {
+        if(workout_options[i].getPrice() < workout_options[cheapest_index].getPrice())
+            cheapest_index = i;
+    }
+
+    return {workout_options[cheapest_index].getId()};
+}
+
 HeavyMuscleCustomer::HeavyMuscleCustomer(std::string name, int id) : Customer(name, id) {
 
 }
 
+std::vector<int> HeavyMuscleCustomer::order(const std::vector<Workout> &workout_options) {
+    return std::vector<int>();
+}
+
 FullBodyCustomer::FullBodyCustomer(std::string name, int id) : Customer(name, id) {
 
+}
+
+std::vector<int> FullBodyCustomer::order(const std::vector<Workout> &workout_options) {
+    return std::vector<int>();
 }
 
 
