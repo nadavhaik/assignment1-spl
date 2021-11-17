f09aec381de3002f2ae31eca73cbf1e1812a9974
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2021-11-17 13:58:34 +0200

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -7,7 +7,14 @@ Order::Order(int id) : trainerId(id) {
 }
 
 void Order::act(Studio &studio) {
-
+    Trainer *t = studio.getTrainer(trainerId);
+    vector<Customer*> customers = t->getCustomers();
+    const vector<Workout> &workoutOptions = studio.getWorkoutOptions();
+    for(Customer *c : customers) {
+        vector<int> customerOrder = c->order(workoutOptions);
+        t->order(c->getId(), customerOrder, workoutOptions);
+    }
+    complete();
 }
 
 std::string Order::toString() const {
