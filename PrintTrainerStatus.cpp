02869a7579563e87d635575542ff703fbd61463e
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2021-11-22 19:28:45 +0200

    Update PrintTrainerStatus.cpp

--- PrintTrainerStatus.cpp
+++ PrintTrainerStatus.cpp
@@ -4,8 +4,13 @@ using namespace std;
 PrintTrainerStatus::PrintTrainerStatus(int id): BaseAction(), trainerId(id) {}
 
 void PrintTrainerStatus::act(Studio &studio) {
+    Trainer *t = studio.getTrainer(trainerId);
+    if(t == nullptr)
+        return;
     int t_salary = 0;
-    cout<< "Trainer "<< trainerId << " status: "<< studio.getTrainer(trainerId)->isOpen()<< endl;
+    string status = t->isOpen() ? "open" : "closed";
+
+    cout<< "Trainer "<< trainerId << " status: "<< status<< endl;
     if(!studio.getTrainer(trainerId)->isOpen()) {
         return;
     }
@@ -15,7 +20,7 @@ void PrintTrainerStatus::act(Studio &studio) {
     }
     cout<<"Orders:"<< endl;
     for(const OrderPair& pair: studio.getTrainer(trainerId)->getOrders()) {
-        cout<<pair.second.getName() << " " << pair.second.getPrice() << " "<<  pair.first<< endl;
+        cout<<pair.second.getName() << " " << pair.second.getPrice() << "NIS "<<  pair.first<< endl;
         t_salary += pair.second.getPrice();
     }
     cout<<"Current Trainer's Salary: " << t_salary<< "NIS"<<endl;
